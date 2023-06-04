19b09bf6e04679e6207d800b898037cb30d98433
José M. Requena Plens <jmrplens@gmail.com>
2023-06-05 01:33:58 +0200

    Delete csp_nonce.js

--- js/csp_nonce.js
+++
@@ -1,5 +0,0 @@
-    // Generate a new random nonce value for every response.
-    const nonce = crypto.randomBytes(16).toString("base64");
-    // Set the strict nonce-based CSP response header
-    const csp = `script-src 'nonce-${nonce}' 'strict-dynamic' https: static.cloudflareinsights.com; connect-src cloudflareinsights.com; object-src 'none'; base-uri 'none';`;
-    response.set("Content-Security-Policy", csp);
