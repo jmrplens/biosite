4cdc9b9ba0a6a3a1bb9285a8e8b29c5ea81d5af9
José M. Requena Plens <jmrplens@gmail.com>
2023-06-05 01:10:06 +0200

    Update csp_nonce.js

--- js/csp_nonce.js
+++ js/csp_nonce.js
@@ -3,7 +3,7 @@ app.get('/', function(request, response) {
     // Generate a new random nonce value for every response.
     const nonce = crypto.randomBytes(16).toString("base64");
     // Set the strict nonce-based CSP response header
-    const csp = `script-src 'nonce-${nonce}' 'strict-dynamic' https:; object-src 'none'; base-uri 'none';`;
+    const csp = `script-src 'nonce-${nonce}' 'strict-dynamic' https: static.cloudflareinsights.com; connect-src cloudflareinsights.com; object-src 'none'; base-uri 'none';`;
     response.set("Content-Security-Policy", csp);
     // Every <script> tag in your application should set the `nonce` attribute to this value.
     response.render(template, { nonce: nonce });
